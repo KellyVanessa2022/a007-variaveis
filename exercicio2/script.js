1604af19f4c8a0801924a73d9219813dcb3966cd
KellyVanessa2022 <kellyvanessa2017@gmail.com>
2022-08-29 18:09:15 -0300

    exercicio concluido

---
+++ exercicio2/script.js
@@ -0,0 +1,13 @@
+let perguntacomida = "você já almoçou hoje?"
+let respostacomida = prompt(perguntacomida)
+
+let perguntacalor = "ai onde vc mora está calor?"
+let respostacalor = prompt (perguntacalor)
+
+let pergunta1 = "já fez todos os exercicios?"
+let resposta2 = prompt(pergunta1)
+ 
+console.log (perguntacomida , respostacomida)
+console.log (perguntacalor , respostacalor)
+console.log (pergunta1 , resposta2)
+
